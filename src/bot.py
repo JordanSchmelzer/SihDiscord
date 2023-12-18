9b43549f3ac9b34cf53fd063350b3c872a42905c
Jordan Schmelzer <jordanschmelzer@gmail.com>
2023-12-17 21:55:59 -0600

    command prefix & yt testing

--- src/bot.py
+++ src/bot.py
@@ -1,44 +1,26 @@
 # bot.py
+# import required dependencies
 import os
 import discord
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-# object that represents a connection to discord
-client = discord.Client(intents=intents)
-
-# pip install -U python-dotenv
-# can read from .env files to get secret variables
-from dotenv import load_dotenv
+from discord.ext import commands
+from discord import FFmpegPCMAudio
+import youtube_dl
+from dotenv import load_dotenv  # pip install -U pyton-dotenv, can read .env files to get secrets (tokens)
+import json
 
 load_dotenv()
 
-# store token credentials in .env (make it a secret)
-# print(DISCORD_TOKEN)
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+# object that represents a connection to discord
+client = commands.Bot(command_prefix='!', intents=intents)
+
+# store token credentials in .env (make it a secret) - gitignore!!!
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-# print('the token is:' + TOKEN)
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my DEV!'
-    )
-
-@client.event
-async def on_message(message):
-    print("i saw a message")
-    print(message.content)
-    # below prevents recursive calls if the bot were to answer itself
-    if message.author == client.user:
-        return
-
-    if message.content == '!respond':
-        response = 'hello'
-        await message.channel.send(response)
-        print('hey I printed something')
 
 @client.event
 # implement on_ready event handler, handles the event when client has established connection and is done prepping data
@@ -54,6 +36,53 @@ async def on_ready():
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hello {member.name}, welcome to my DEV environment!'
+    )
+
+
+@client.event
+async def on_message(message):
+    print("i saw a message")
+    print(message.content)
+    # below prevents recursive calls if the bot were to answer itself
+    if message.author == client.user:
+        return
+
+    if message.content == 'respond':
+        response = 'hello'
+        await message.channel.send(response)
+        print('hey I printed something')
+
+@client.command()
+async def play(ctx,url:str):
+    channel = ctx.message.author.voice.channel
+    voice = await channel.connect()
+
+    # download the youtube video
+    ydl_opts = {
+         'format': 'bestaudio/best',
+         'postprocessors':[{
+         'key': 'FFmpegExtractAudio',
+         'preferredcodec': 'mp3',
+         'preferredquality': '192',
+         }]
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file,"song.mp3")
+
+
+    source = FFmpegPCMAudio('song.mp3')
+    player = voice.play(source)
 
 # launches the client using the bots token
 client.run(TOKEN)
