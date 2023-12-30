cf3467b419ed04f765b58b9e818d6d76bdf6af0f
Jordan Schmelzer <jordanschmelzer@gmail.com>
2023-12-29 18:19:33 -0600

    relative paths lesson learned

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -1,6 +1,23 @@
 from discord.ext import commands
 import discord
 import youtube_dl
+import os
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
 
 
 class Music(commands.Cog):
@@ -12,29 +29,38 @@ class Music(commands.Cog):
         print(f'{self} connected to discord. ready for further action')
 
     @commands.command()
-    async def p(self, ctx):
-        '''
+    async def p(self, ctx, url: str):
         ydl_opts = {
             'format': 'bestaudio/best',
+            'extractaudio': True,
             'noplaylist': True,
+            'nocheckcertificate': True,
+            'logtostderr': False,
+            'ignoreerrors': False,
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
-            }]
+            }],
+            'no_warnings': True,
+            'logger': MyLogger(),
+            'progress_hooks': [my_hook],
         }
-        print("done setting ydl options")
+
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-            print('im downloading')
-        for file in os.listdir("./"):
-            if file.endswith(".mp3"):
-                if file != 'song.mp3':
-                    os.rename(file, "song.mp3")
-                    print('i renamed the file')
-        '''
 
-        audio_source = 'src/cogs/song.mp3'  # free use as long as credited, credit! freestockmusic.com
+        for file in os.listdir('./'):
+            print(file)
+
+        path = './'
+        dest = 'src/songs/that.mp3'
+        for file in os.listdir(path):
+            if file.endswith(".mp3"):
+                print(file)
+                os.rename(path + file, dest)
+
+        audio_source = dest
         src = discord.FFmpegPCMAudio(audio_source)
         voice = await ctx.channel.connect()
         voice.play(src, after=lambda e: print('Player error: %s' % e) if e else None)
@@ -54,6 +80,7 @@ class Music(commands.Cog):
         voice = server.voice_client
         voice.resume()
         print('resume')
+
 
 # The setup function below is necessary. Remember we give bot.add_cog() the name of the class in this case MembersCog.
 # When we load the cog, we use the name of the file.
